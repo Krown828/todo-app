1bf3a2d4197f65a1d7ea2103c60f9cd24818035c
Tony <161581263+tony76122@users.noreply.github.com>
2026-06-04 21:40:09 +0200

    feat: implement task creation (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+  const addTask = () => {
+    if (input.trim() !== "") {
+      const newTask = {
+        id: Date.now(),
+        text: input,
+        complete: false,
+      };
+      setTasks([...tasks, newTask]);
+      setInput("");
+    }
+    console.log(tasks);
+  };
   return (
     <>
       <div id="center">
         <h1>Todo App</h1>
-        <input type="text" name="task" id="task" placeholder='Enter a task' />
-        <button type="button">
+        <input
+          type="text"
+          name="task"
+          id="task"
+          placeholder="Enter a task"
+          value={input}
+          onChange={onChange}
+        />
+        <button type="button" onClick={addTask}>
           Add
         </button>
         <ul>
-          <li>Learn React</li>
-          <li>Practice Git</li>
+          {tasks.map((task, index) => (
+            <li key={index}>{task.text}</li>
+          ))}
         </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
